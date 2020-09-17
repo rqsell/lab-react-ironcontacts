2b0e2b7217fc1ce5a8d4f8d915b0a9d3b28fbada
rqsell <68514875+rqsell@users.noreply.github.com>
2020-09-17 12:38:55 -0400

    rebecca's v

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+//IT 1
+//get the first five contacts in an array
+//display the contacts in a table: picture, name, popularity
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">{DisplayFive()}</div>;
 }
 
+function DisplayFive(props) {
+  return contacts
+    .map((eachcontact) => {
+      return (
+        <table id="contact">
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+
+          <tr>
+            <td>
+              <img id="contactimage" src={eachcontact.pictureUrl} />
+            </td>
+            <td> {eachcontact.name}</td>
+            <td>{eachcontact.popularity}</td>
+          </tr>
+        </table>
+      );
+    })
+    .splice(0, 5);
+  // for (let i = 0; i < contacts.length; i++) {
+  //   if (i < 5 === true) {
+  //     return <div>{contacts[i].name}</div>;
+  //   }
+  // }
+}
 export default App;
